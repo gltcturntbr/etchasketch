4b166908b1ad61ccb6caa2539b348f594a8940ea
coder123 <hisup197@gmail.com>
2024-05-23 23:24:24 -0400

    Add etch a sketch grid

--- sauce.js
+++ sauce.js
@@ -1,3 +1,21 @@
+function createDrawing() {
+  /*
+    Creates an individual pixel, for drawing
+    :D
+  */
+  const drawingsquare = document.createElement("div");
+  drawingsquare.style.borderRadius = "5px";
+  drawingsquare.style.height = "25px";
+  drawingsquare.style.width = "25px";
+  drawingsquare.style.backgroundColor = "white";
 
-const drawingsquare = document.createElement("div")
-const container = document.querySelector(".bigthing")
+  return drawingsquare;
+}
+
+const container = document.querySelector(".bigthing");
+
+for (let i = 0; i < 240; i++) {
+  container.appendChild(createDrawing());
+}
+
+
