324fe04602856a8d0a2b1513133a65eb4cb52375
coder123 <hisup197@gmail.com>
2024-05-23 23:45:12 -0400

    Add etch a sketch functionality

--- sauce.js
+++ sauce.js
@@ -5,17 +5,23 @@ function createDrawing() {
   */
   const drawingsquare = document.createElement("div");
   drawingsquare.style.borderRadius = "5px";
-  drawingsquare.style.height = "25px";
-  drawingsquare.style.width = "25px";
-  drawingsquare.style.backgroundColor = "white";
+  drawingsquare.style.height = "20px";
+  drawingsquare.style.width = "20px";
+  drawingsquare.style.backgroundColor = "green";
+  drawingsquare.className = "drawing";
 
   return drawingsquare;
 }
 
 const container = document.querySelector(".bigthing");
 
-for (let i = 0; i < 240; i++) {
+for (let i = 0; i < 340; i++) {
   container.appendChild(createDrawing());
 }
 
-
+container.addEventListener("mouseover", function (e) {
+  if ( e.target.className != "drawing"){
+    return
+  }
+  e.target.style.backgroundColor = "gray"
+});
